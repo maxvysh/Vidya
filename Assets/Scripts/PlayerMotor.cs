4182334d5bd82a60c4d44685a6c0034d4a87ce86
maxvysh <45989000+maxvysh@users.noreply.github.com>
2023-03-28 18:13:07 -0400

    Movement is done!

---
+++ Assets/Scripts/PlayerMotor.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMotor : MonoBehaviour
+{
+    private CharacterController controller;
+    private Vector3 playerVelocity;
+    private bool IsGrounded;
+    public float speed = 5f;
+
+    public float gravity = -9.8f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        IsGrounded = controller.isGrounded;
+    }
+    //receive the inputs for our InputManager.cs and apply them to our character controller.
+    public void ProcessMove(Vector2 input)
+    {
+        Vector3 moveDirection = Vector3.zero;
+        moveDirection.x = input.x;
+        moveDirection.z = input.y;
+        controller.Move(transform.TransformDirection(moveDirection) * speed * Time.deltaTime);
+        playerVelocity.y += gravity * Time.deltaTime;
+        if (IsGrounded && playerVelocity.y < 0)
+        {
+            playerVelocity.y = -2f;
+        }
+        controller.Move(playerVelocity * Time.deltaTime);
+        Debug.Log(playerVelocity.y);
+    }
+}
